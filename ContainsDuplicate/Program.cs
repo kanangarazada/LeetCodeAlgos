633c18ef5e92946d62b01157e04842046b4a0bb6
Kanan Garazada <kanangarayev02@gmail.com>
2023-05-02 10:13:59 +0400

    Add project files.

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,11 @@
+﻿static bool ContainsDuplicate(int[] nums)
+{
+    HashSet<int> set = new HashSet<int>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (set.Contains(nums[i])) return true;
+        set.Add(nums[i]);
+    }
+
+    return false;
+}
