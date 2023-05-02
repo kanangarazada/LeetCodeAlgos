acb406be10c2be82cefdf27eede24ff12d3836c3
Kanan Garazada <kanangarayev02@gmail.com>
2023-05-02 11:57:01 +0400

    two sum solved with hashMap

---
+++ TwoSum/Program.cs
@@ -0,0 +1,19 @@
+﻿static int[] TwoSum(int[] nums, int target)
+{
+    Dictionary<int, int> prevMap = new ();
+
+	int diff = 0;
+
+	for (int i = 0; i < nums.Length; i++)
+	{
+		diff = target - nums[i];
+
+		if(prevMap.ContainsKey(diff))
+		{
+			return new int[] { prevMap[diff], i };
+		}
+
+		prevMap[nums[i]] = i;
+	}
+	return new int[] { 0, 0};
+}
