770690599f58027371e489b82319953e76edc9d0
Kanan Garazada <kanangarayev02@gmail.com>
2023-05-03 11:58:58 +0400

    group anagrams solved with hashMap

---
+++ GroupAnagrams/Program.cs
@@ -0,0 +1,22 @@
+﻿static IList<IList<string>> GroupAnagrams(string[] strs)
+{
+	Dictionary<string, IList<string>> result = new();
+
+	foreach (string s in strs)
+	{
+		char[] hash = new char[26];
+		foreach (char c in s)
+		{
+			hash[c - 'a']++;
+        }
+
+		string key = new string(hash);
+        if (!result.ContainsKey(key))
+		{
+			result[key] = new List<string>();
+		}
+		result[key].Add(s);
+	}
+
+	return result.Values.ToList();
+}
