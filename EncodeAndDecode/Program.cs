90e7a5d2adaa5cf1aec9d9696c66d13c0ee6ef18
Kanan Garazada <kanangarayev02@gmail.com>
2023-05-05 10:41:36 +0400

    encode&decode strings

---
+++ EncodeAndDecode/Program.cs
@@ -0,0 +1,37 @@
+﻿//Lintcode 659
+//Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+static string Encode(IList<string> strs) 
+{
+	string result = "";
+
+    foreach (string str in strs)
+    {
+        result += str.Length + "#" + str;
+    }
+
+    return result;
+}
+
+
+static IList<string> Decode(string s)
+{
+    var result = new List<string>();
+
+    int i = 0;
+    while(i < s.Length)
+    {
+        int j = i;
+        while (s[j] != '#')
+        {
+            j++;
+        }
+
+        //get the length of encoded string
+        int.TryParse(s.Substring(i, j - i), out var len);
+        j++;
+        result.Add(s.Substring(j, len));
+        i = j + len;
+    }
+
+    return result;
+}
