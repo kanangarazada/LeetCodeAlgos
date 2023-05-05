2b575f915bb6cb559722540b2e8339205fcdcb09
Kanan Garazada <kanangarayev02@gmail.com>
2023-05-05 12:34:07 +0400

    longest consecutive sequence

---
+++ LongestConsecutiveSequence/Program.cs
@@ -0,0 +1,23 @@
+﻿//LeetCode 128
+//Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+static int LongestConsecutive(int[] nums)
+{
+    var set = new HashSet<int>(nums);
+    int longest = 0;
+
+    foreach (var n in set)
+    {
+        //check if it is the start of consecutive set
+        if (!set.Contains(n - 1))
+        {
+            int length = 0;
+            while (set.Contains(n + length))
+            {
+                length++;
+                longest = Math.Max(longest, length);
+            }
+        }
+    }
+    return longest;
+}
