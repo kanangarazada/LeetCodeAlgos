83094dfb60b38e9c363f454a0d2fb3b0ff614c90
Kanan Garazada <kanangarayev02@gmail.com>
2023-05-08 09:49:07 +0400

    valid parantheses solved with stack

---
+++ ValidParantheses/Program.cs
@@ -0,0 +1,30 @@
+﻿//LeetCode 20
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+static bool IsValid(string s)
+{
+    var stack = new Stack<char>();
+
+    //keys are closing parentheses
+    Dictionary<char, char> closeToOpen = new()
+    {
+        [')'] = '(',
+        [']'] = '[',
+        ['}'] = '{'
+    };
+
+    foreach (var item in s)
+    {
+        if (!closeToOpen.ContainsKey(item))
+        {
+            //add opening parentheses to stack
+            stack.Push(item);
+        }
+        //Pop the opening character if it is closed 
+        else if (stack.Count == 0 || stack.Pop() != closeToOpen[item])
+        {
+            return false;
+        }
+    }
+
+    return stack.Count == 0;
+}
